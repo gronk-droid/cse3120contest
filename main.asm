ba23eaa29de884cd3805689e7dd9d7adaaf7000e
Grant Butler <grantbutler7@gmail.com>
2022-04-21 00:08:56 -0400

    literally what

--- main.asm
+++ main.asm
@@ -2,6 +2,7 @@
 ; CSE3210 Contest #1
 
 INCLUDE Irvine32.inc
+INCLUDE Macros.inc
 
 .data
 random_string BYTE 41 DUP(?), 0
@@ -27,7 +28,7 @@ instruction_confirm_string BYTE "Hit any key to continue!", 0
 
 random_word_header BYTE "Random String: ", 0
 
-size_selection_string BYTE "Choose the difficulty (1-40 letters): ", 0
+size_selection_string BYTE "Choose the difficulty (1-40 letters/words): ", 0
 
 get_input_string BYTE "Enter the above characters: ", 0
 
@@ -49,13 +50,25 @@ start_seconds DWORD ?
 
 end_seconds DWORD ?
 
-choose_game_mode_string BYTE "Choose game mode (1 = Letters, 2 = Any Character): ", 0
+choose_game_mode_string BYTE "Choose game mode (1 = Letters, 2 = Any Character, 3 = Random Words): ", 0
 
 user_game_mode DWORD ?
 
 seconds_display_string BYTE "Milliseconds to write the random string: ", 0
 
 invalid_input_string BYTE "USER INPUT INVALID!!!", 0
+
+BUFSIZE = 55000
+NUM_WORDS = 4961
+words_file_handle HANDLE ?
+words_file_name BYTE "C:\Users\gronk-droid\gh\fit2022spring\cse3120\contest\bank.txt"
+words_buffer BYTE BUFSIZE DUP(?)
+bytesRead DWORD ?
+
+max_array BYTE 40 DUP(?)
+temp_word BYTE 60 DUP(?), 0
+
+file_error_string BYTE "File read error!", 0
 
 .code
     main PROC
@@ -64,11 +77,11 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
         call WriteString
 
         ; Set console title name
-        INVOKE SetConsoleTitle, ADDR game_title 
+        INVOKE SetConsoleTitle, ADDR game_title
 
         ; wait (seconds x 1000) and clear everything
         INVOKE Sleep, 1000
-        call Clrscr 
+        call Clrscr
 
         ; Print Gameplay Info
         mov edx, offset instruction_string
@@ -82,7 +95,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
         ; Print instructions to continue
         mov edx, offset instruction_confirm_string
         call WriteString
-        
+
         ; Wait for user to input to confirm instructions read
         wait_for_key:
             ; Delay to not miss
@@ -108,13 +121,13 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                 inc ebx
                 cmp ebx, 40
                 jle reset_word
-            
+
             ; Reset string size
             mov string_size, 000000000h
 
             ; Wait (seconds x 1000) and clear everything
             INVOKE Sleep, 1000
-            call Clrscr 
+            call Clrscr
 
             ; Get user input for game mode
             user_game_mode_loop:
@@ -122,11 +135,11 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                 call WriteString
                 call readInt
                 mov user_game_mode, eax
-                
+
                 ; Compare game mode option and jump if it is correct/incorrect
                 cmp user_game_mode, 0
                 jbe error_jump ; Out of range
-                cmp user_game_mode, 3
+                cmp user_game_mode, 4
                 jae error_jump ; Out of range
 
                 jmp get_string_size ; Inbounds
@@ -139,7 +152,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                     call Clrscr
                     jne user_game_mode_loop
 
-            
+
             get_string_size:
                 ; Get user input for number of chars
                 mov edx, offset size_selection_string
@@ -157,6 +170,9 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                 cmp user_game_mode, 2
                 je make_random_char
 
+                cmp user_game_mode, 3
+                je make_random_words
+
                 make_random_letter:
 	                call GenerateReandomLetter
                     jmp continute_random_string
@@ -165,19 +181,23 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                     call GenerateReandomChar
                     jmp continute_random_string
 
+                make_random_words:
+                    call GenerateRandomWords
+                    jmp countdown
+
                 continute_random_string:
                     ; add it to the string
                     mov random_string[edx], al
-	            
+
                     ; move edx to the next location
                     inc edx
-                
+
                     ; string_size decides number of chars
                     cmp edx, string_size
-	        
+
                     ; jump while we are below, this allows us to add one extra space at the end of the newline
                     jb make_random_string
-            
+
                     ; add the newline to the end
                     mov random_string[edx], 0Ah
 
@@ -191,7 +211,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                 INVOKE Sleep, 1000 ; uses EAX so we put it on the stack for keeping
                 pop eax
                 cmp eax, 0
-                jne countdown   
+                jne countdown
 
             ; Print the random word out
             mov edx, OFFSET random_word_header ; print the header
@@ -205,7 +225,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
 
             ; Save current time for total time
             call GetMseconds
-            mov  start_seconds, eax  
+            mov  start_seconds, eax
 
             ; get user input
             mov  edx, OFFSET user_input_string
@@ -225,7 +245,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
 
                 jne wrong_letter
                 je correct_letter
-            
+
                 ; If wrong, change background color to red
                 wrong_letter:
                    ; Line up to the correct character
@@ -233,7 +253,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
 
                     mov al, LENGTHOF get_input_string
                     sub al, 1 ; remove null char
-                
+
                     add dl, al
                     mov dh, 6  ; row
                     call Gotoxy
@@ -249,7 +269,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                     ; reset the color
                     mov  eax, white + (black * 16)
                     call SetTextColor
-                
+
                     jmp end_loop
 
                 ; If right, change background color to green
@@ -259,7 +279,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
 
                     mov al, LENGTHOF get_input_string
                     sub al, 1 ; remove null char
-                
+
                     add dl, al
                     mov dh, 6  ; row
                     call Gotoxy
@@ -276,9 +296,9 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                     mov  eax, white + (black*16)
                     call SetTextColor
 
-                    ; Add one correct letter to the count 
+                    ; Add one correct letter to the count
                     inc bp
-                
+
                     jmp end_loop
 
                 end_loop:
@@ -286,13 +306,13 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
                     inc ebx
                     cmp ebx, string_size
                     jb check_if_correct_loop
-        
+
             call Crlf
 
             ; Print number of correct letters typed
             mov edx, OFFSET correct_number_string
             call WriteString
-            
+
             push eax
             mov ax, bp
             call WriteInt
@@ -303,7 +323,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
             ; Print number of incorrect letters typed
             mov edx, OFFSET incorrect_number_string
             call WriteString
-            
+
             push eax
             mov eax, string_size
             sub ax, bp
@@ -315,7 +335,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
             ; Print user time to write the string
             mov edx, OFFSET seconds_display_string
             call WriteString
-            
+
             push eax
             mov eax, end_seconds
             sub eax, start_seconds
@@ -323,7 +343,7 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
             pop eax
 
             call Crlf
-            
+
             ; Loop game!!!!
             ; Put user input prompt on screen
             mov edx, OFFSET play_again_prompt
@@ -360,13 +380,13 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
         call RandomRange
         cmp eax, 1
         je uppercase_letter ; 1 = uppercase
-        jne lowercase_letter 
+        jne lowercase_letter
 
         uppercase_letter:
             ; make it uppercase
             add ebx, 65
             jmp finish_GenerateReandomLetter
-        
+
         lowercase_letter:
             ; make it lowercase
             add ebx, 97
@@ -378,18 +398,95 @@ invalid_input_string BYTE "USER INPUT INVALID!!!", 0
     GenerateReandomLetter ENDP
 
     GenerateReandomChar PROC
-    ; This proc returns AL with a random chararacter
+        ; This proc returns AL with a random chararacter
 
-    ; Choose a random char
-    mov eax, 94
-    call RandomRange
+        ; Choose a random char
+        mov eax, 94
+        call RandomRange
 
-    ; Move to ebx for storage
-    mov ebx, eax
+        ; Move to ebx for storage
+        mov ebx, eax
 
-    ; Generate a random number and compare to choose upper/lower case
-    add ebx, 32
-    mov eax, ebx ; send it back in al properly
-    ret
+        ; Generate a random number and compare to choose upper/lower case
+        add ebx, 32
+        mov eax, ebx ; send it back in al properly
+        ret
     GenerateReandomChar ENDP
+
+
+    GenerateRandomWords PROC
+        ; this grabs and pulls random words from a txt file
+        mov edx, OFFSET words_file_name  ; move file name to edx
+        call OpenInputFile
+        mov words_file_handle, eax  ; move file handle to variable
+
+        ; grab the words from the file.
+        mov eax, words_file_handle
+        mov edx, OFFSET words_buffer
+        mov ecx, BUFSIZE
+        call ReadFromFile
+        mov bytesRead, eax
+
+        ; make an array of random words from the set
+
+        make_array_words:
+
+            push words_buffer
+            call GetNumLines
+            call RandomRange
+            inc eax
+
+            mov ebx, 0 ; loop value
+            ; check if array is finished
+
+            cmp eax, string_size
+            jge array_done
+
+            ; check letter by letter until
+            mov al, words_buffer[ebx]
+
+
+
+
+            ; increment counter
+            inc ebx
+
+        mov edx, OFFSET words_buffer
+        loop_buffer:
+
+
+        array_done:
+        ret
+    GenerateRandomWords ENDP
+
+    GetNumLines PROC src:DWORD
+    ; count ascii value for CR → 13
+    mov edx, [esp + 4]
+    sub edx, 1
+    xor eax, eax
+    jmp loop_1
+
+    increment:
+        inc eax
+
+    loop_1:
+        add edx, 1
+        movzx ecx, BYTE PTR [edx]
+        cmp ecx, 13
+        je increment
+        test ecx, ecx
+        jz loop_2
+
+        add edx, 1
+        movzx ecx, BYTE PTR [edx]
+        cmp ecx, 13
+        je increment
+        test ecx, ecx
+        jnz loop_1
+
+    loop_2:
+        ret 4
+
+    GetNumLines ENDP
+
 END main
